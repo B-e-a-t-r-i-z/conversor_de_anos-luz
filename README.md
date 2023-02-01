280447a36fcbbb36a7afcc06cbe764ecc8f4e093
Beatriz <beatriz.almeida2001@outlook.com>
2023-01-31 23:49:30 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# conversor_de_anos-luz
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/B-e-a-t-r-i-z/pen/poZxEqa](https://codepen.io/B-e-a-t-r-i-z/pen/poZxEqa).
+
+Um conversor de Anos-luz para metros
---
+++ src/script.js
@@ -0,0 +1,14 @@
+var valorEmAnosLuz = 2;
+var medidaEmAnosLuz = 9.46073047e15;
+var calculo = valorEmAnosLuz * medidaEmAnosLuz;
+var nome = "Beatriz";
+
+alert(
+  "Olá " +
+    nome +
+    "! para " +
+    valorEmAnosLuz +
+    " Anos-Luz fica em: " +
+    calculo +
+    " m"
+);
